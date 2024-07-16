24071d0de0ffdabd14cb14932f3421defd24a247
AndrusRoman <andrusroman911@gmail.com>
2024-07-16 12:14:01 -0300

    atualizaçção

---
+++ ListarCliente.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="cssListar.css">
+</head>
+<body>
+    <?php
+        include('includes/conexao.php');
+        $sql = "SELECT * FROM cliente";
+        $result = mysqli_query($con, $sql);
+    ?>
+    <h1>Consulta de Cliente</h1>
+    <button><a href="index.html">Retornar</a></button>
+    <table align="center" border="1" width="500">
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Senha</th>
+            <th>Alterar</th>
+            <th>Deletar</th>
+        </tr>
+        <?php
+            while($row = mysqli_fetch_array($result)){
+                echo "<tr>";
+                echo "<td>".$row['id']."</td>";
+                echo "<td>".$row['nome']."</td>";
+                echo "<td>".$row['email']."</td>";
+                echo "<td>".$row['senha']."</td>";
+                echo "<td><a href='alteraCliente.php?id=".$row['id']."'>Alterar</a></td>";
+                echo "<td><a href='deletaCliente.php?id=".$row['id']."'>Deletar</a></td>";
+                echo "</td>";
+            }
+        ?>
+    </table>
+</body>
+</html>
